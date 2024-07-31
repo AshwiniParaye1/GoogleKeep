b31ffd9ddb5427ecb805ab00f6fc2ff37fabe900
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-31 16:05:29 +0530

    added keep icon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
+import keep from "./assets/icons/keep.svg";
 
 function Home() {
   return (
     <div className="home">
-      <h1>
-        <a href="/menu">Menu</a>
-      </h1>
+      {/* <h1>Navbar</h1> */}
+      <a href="/">
+        <img src={keep} alt="Keep Icon" />
+      </a>
       <div className="homeContainer">
         <h1>
-          <a href="/navbar">Navbar</a>
+          <a href="/menu">Menu</a>
         </h1>
         <h1>
           <a href="/notes">Notes</a>
